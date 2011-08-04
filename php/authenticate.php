1622683c486ab00f4d30c00202ab85e623e0e8b4
Nikolai Kotchetkov <motorro@gmail.com>
2011-08-04 09:01:11 +0400

    Authenticate stub, Upload stub update

---
+++ php/authenticate.php
@@ -0,0 +1,37 @@
+﻿<?php
+/********************************************************** 
+ *                Authorization stub                      *
+ * Checks for valid authorization ticket passed.          *
+ * If ticket is valid, returns OK and album title         *
+ *********************************************************/ 
+
+define ('STATUS_OK', 0);
+define ('STATUS_ERROR', 100);
+define ('STATUS_ERROR_EMPTY_TOKEN', STATUS_ERROR + 10); 
+define ('STATUS_ERROR_INVALID_TOKEN', STATUS_ERROR + 20); 
+ 
+$token = isset($_POST["token"]) ? $_POST["token"] : "";
+if ("" == $token) {
+	result("Error", STATUS_ERROR_EMPTY_TOKEN, "Empty token.");
+} 
+
+//Authorization error test
+if ("BadOne" == $token) {
+	result("Error", STATUS_ERROR_INVALID_TOKEN, "Invalid token");
+}
+
+result("Ok", STATUS_OK, "Authorized", "<order><name>Альбом $token</name></order>");
+
+function result($status, $code, $message, $xmlData = null) {
+	header ("Content-type: text/xml; charset=utf-8");
+	print ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+	print ("<result>\n");
+	print ("\t<status>$status</status>\n");
+	print ("\t<code>$code</code>\n");
+	print ("\t<message><![CDATA[$message]]></message>\n");
+	if ($xmlData) {
+		print ($xmlData);
+	}
+	print ("</result>");
+}
+?>
--- php/upload.php
+++ php/upload.php
@@ -1,5 +1,13 @@
 <?php
-//Uploader stub
+/********************************************************** 
+ *                   Uploader stub                        *
+ * Checks for valid parameters and file.                  *
+ * Returns passed parameters in reply.                    *
+ *********************************************************/ 
+define ('STATUS_OK', 0);
+define ('STATUS_ERROR', 100);
+define ('STATUS_ERROR_NO_FILE', STATUS_ERROR + 10); 
+define ('STATUS_ERROR_UPLOAD_ERROR', STATUS_ERROR + 20); 
 
 $order = isset($_POST["order"]) ? $_POST["order"] : "";
 $format = isset($_POST["format"]) ? $_POST["format"] : "";
@@ -10,22 +18,26 @@ $exifDate = isset($_POST["exifDate"]) ? $_POST["exifDate"] : "";
 $foldername = dirname($_SERVER['SCRIPT_FILENAME'])."/images/";
 
 if (!isset($_FILES['Filedata'])) {
-	result("Error", "No file transfered");
+	result("Error", STATUS_ERROR_NO_FILE, "No file transfered");
 	exit();
 }
 
 if (move_uploaded_file($_FILES['Filedata']['tmp_name'], $foldername.$_FILES['Filedata']['name'])) {
-	result("Ok", "Uploaded. Order: $order, format: $format, filename: $fileName, orientation: $orientation, date: $exifDate");
+	result("Ok", STATUS_OK, "Uploaded. Order: $order, format: $format, filename: $fileName, orientation: $orientation, date: $exifDate");
 } else {
-	result("Error", "Upload error");
+	result("Error", STATUS_ERROR_UPLOAD_ERROR, "Upload error");
 }
 
-function result($status, $message) {
+function result($status, $code, $message, $xmlData = null) {
 	header ("Content-type: text/xml; charset=utf-8");
 	print ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 	print ("<result>\n");
 	print ("\t<status>$status</status>\n");
+	print ("\t<code>$code</code>\n");
 	print ("\t<message><![CDATA[$message]]></message>\n");
+	if ($xmlData) {
+		print ($xmlData);
+	}
 	print ("</result>");
 }
 ?>
